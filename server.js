0bd8503da702b177b8999fe1739b83c40cd23cb7
White Lily <mmacalino03@gmail.com>
2018-09-19 19:06:08 -0400

    cleaned server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
-var app - express();
+var app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
